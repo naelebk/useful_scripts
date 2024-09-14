10d155f4f4d289a3f24f51dbe004139a1b3d3fe3
Naël <nael.embarki34@gmail.com>
2024-09-14 11:18:15 +0200

    Mise à jour de useful_scripts

--- makefile_latex.sh
+++ makefile_latex.sh
@@ -1,7 +1,6 @@
-#!/bin/dash
+#!/bin/bash
 # Ce programme a pour objectif de générer un makefile automatiquement,
-# en se basant sur les fichiers .c présent dans le répertoire courant
-# (autre script pour les fichiers en C++)
+# en se basant sur les fichiers .tex présent dans le répertoire courant
 
 # PS : Dans ce script, le makefile peut être créer dans un répertoire parent,
 # mais pas dans un répertoire enfant !
@@ -11,13 +10,34 @@ PURPLE='\033[0;35m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+# extension pour le fichier .tex
+extension=".tex"
+# extension pour le fichier de sortie
+out=".pdf"
+
 saut_de_ligne() {
 	echo >> "$1"
 }
 
+check_cmd() {
+    if [[ $? -eq 0 ]] || [[ $? -eq 1 ]]; then
+        if [[ -z "$1" ]]; then 
+            echo -e "${GREEN}OK.${NC}"
+        else 
+            echo -e "${GREEN}OK pour $1.${NC}"
+        fi
+    else
+        if [[ -z "$1" ]]; then 
+            echo -e "${RED}ERREUR !${NC}"
+        else
+            echo -e "${RED}ERREUR pour $1.${NC}"
+        fi
+    fi
+}
+
 if [ "$#" -eq 0 ]; then
 	# On effectue la saisie pour savoir dans quel répertoire on génère le makefile
-	echo -n "${YELLOW}Quel répertoire pour créer le makefile ? (tapez \"pwd\" ou \".\" pour le répertoire courant)${NC} "
+	echo -ne "${YELLOW}Quel répertoire pour créer le makefile ? (tapez \"pwd\" ou \".\" pour le répertoire courant)${NC} "
 	read rep
 	case "$(echo "$rep" | tr '[:upper:]' '[:lower:]')" in
 		pwd*|".")
@@ -25,8 +45,8 @@ if [ "$#" -eq 0 ]; then
 			;;
 		*)
 			if [ ! -d "$rep" ]; then
-				echo "${RED}Erreur, la saisie ne correspond à aucun répertoire.${NC}"
-				echo "${RED}--------------- Arrêt du script ---------------${NC}"
+				echo -e "${RED}Erreur, la saisie ne correspond à aucun répertoire.${NC}"
+				echo -e "${RED}--------------- Arrêt du script ---------------${NC}"
 				sleep 2
 				exit 1
 			else
@@ -38,81 +58,64 @@ else
 	if [ -d "$1" ]; then
 		repertoire="$1"
 	else
-		echo "${RED}Erreur, répertoire invalide${NC}"
+		echo -e "${RED}Erreur, répertoire invalide ('$repertoire')${NC}"
 		exit 1
 	fi
 fi
 # On affiche le répertoire utilisé
 # L'utilisateur a encore la possibilité d'annuler son opération
-echo "${GREEN}Répertoire utilisé : $repertoire (CTRL + C POUR STOPPER ICI).${NC}"
+echo -e "${GREEN}Répertoire utilisé : $repertoire (CTRL + C POUR STOPPER ICI).${NC}"
 sleep 3
-extension=".tex"
+
+# Recherche d'un fichier $extension dans $repertoire (au moins 1)
 found=false
 for file in "$repertoire"/*; do
-	if [ -f "$file" ] && echo "$file" | grep -E "$extension$"; then
-		found=true
-		break
-	fi
+	test -f "$file" && echo "$file" | grep -Eq "$extension\$" && found=true && break
 done
+
 if [ "$found" = false ]; then
 	echo "${RED}Il n'y a aucun fichier $extension dans $repertoire.${NC}"
 	echo "${RED}--------------- Arrêt du script ---------------${NC}"
-    sleep 2
+	sleep 2
 	exit 3
 fi
 ma="$repertoire/Makefile"
 if [ -f "$ma" ]; then
+	echo -ne "${YELLOW}Suppression de l'ancien Makefile.....${NC} "
 	rm -f "$ma"
-	if [ "$?" -eq 0 ]; then
-		echo "${GREEN}Ancien Makefile supprimé avec succès !${NC}"
-		sleep 1
-	else
-		echo "${RED}Erreur lors de la suppression de l'ancien Makefile. ${NC}"
-		echo "${RED}--------------- Arrêt du script ---------------${NC}"
-    	sleep 2
-		exit 4
-	fi
+	check_cmd ""
 fi
+
+echo -ne "${YELLOW}Création du Makefile.....${NC} "
 touch "$ma"
-if [ "$?" -ne 0 ]; then
-	echo "${RED}Erreur lors de la création du Makefile (pas les permissions requises). ${NC}"
-	echo "${RED}--------------- Arrêt du script ---------------${NC}"
-    sleep 2
-	exit 5
-fi
+check_cmd ""
+echo -ne "${YELLOW}Permissions du Makefile.....${NC} "
 chmod 755 "$ma"
-if [ "$?" -ne 0 ]; then
-	echo "${RED}Erreur lors de l'attribution des permissions du Makefile. ${NC}"
-	echo "${RED}--------------- Arrêt du script ---------------${NC}"
-    sleep 2
-	exit 6
-fi
+check_cmd ""
+
 echo "LATEX = pdflatex" >> "$ma"
+
 all_files=""
-for file in "$repertoire"/*$extension; do
+all_rules=""
+for file in "$repertoire"/*${extension}; do
     if [ -f "$file" ]; then
-        filename=$(basename "$file" | cut -d. -f1) # Récupère le nom du fichier sans l'extension
-        all_files="$all_files $filename.pdf"
+        filename=$(basename "$file" | cut -d. -f1)
+        all_files+="${filename}${out} "
+		all_rules+="${filename}${out}: ${filename}${extension}\n\n"
     fi
 done
 echo "TARGETS = $all_files" >> "$ma"
 # Règle 'all'
 echo "all:\${TARGETS}" >> "$ma"
 saut_de_ligne "$ma"
-echo "%.pdf: %.tex %.aux\n\t\${LATEX} \$<" >> "$ma"
+echo -e "%${out}: %.tex %.aux\n\t\${LATEX} \$<" >> "$ma"
 saut_de_ligne "$ma"
-echo "%.aux: %.tex\n\t\${LATEX} \$<" >> "$ma"
+echo -e "%.aux: %.tex\n\t\${LATEX} \$<\n" >> "$ma"
 # Ajout de chaque règle 
-for file in "$repertoire"/*$extension; do
-    if [ -f "$file" ]; then
-        filename=$(basename "$file" | cut -d. -f1) # Récupère le nom du fichier sans l'extension
-        echo "$filename.pdf: $filename$extension" >> "$ma"
-        saut_de_ligne "$ma"
-    fi
-done
-echo "clean:\n\trm -f *.aux *.log *.toc *.lof\n\trm -f *.bbl *.blg\n\trm -f *.nav *.out *.snm\n\trm -f *.vrb" >> "$ma"
+echo -e "$all_rules" >> "$ma"
+echo -e "clean:\n\trm -f *.aux *.log *.toc *.lof\n\trm -f *.bbl *.blg\n\trm -f *.nav *.out *.snm\n\trm -f *.vrb" >> "$ma"
 saut_de_ligne "$ma"
-echo "mrproper: clean\n\trm -f \${TARGETS}" >> "$ma"
+echo -e "mrproper: clean\n\trm -f \${TARGETS}" >> "$ma"
 saut_de_ligne "$ma"
 echo "remake: mrproper all" >> "$ma"
-echo "${GREEN}Makefile généré avec succès !${NC}"
+echo -e "${GREEN}Makefile généré avec succès !${NC}"
